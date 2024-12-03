3772ce6438b3a231654afaa66dd6935abca0c275
rasyadanfz <18221103@std.stei.itb.ac.id>
2024-12-03 15:57:10 +0700

    style: update dark style on in dev homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex h-full w-full items-center justify-center dark:bg-black">
       <div className="pt-4 text-center dark:bg-black">
         <h1 className="text-4xl font-bold dark:text-white">Coming Soon!</h1>
         <div className="text-md mb-1 dark:text-white">In development</div>
