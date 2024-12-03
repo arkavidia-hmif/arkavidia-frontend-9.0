674f900fef964e5d8b1c6ceac258686420ae4618
rasyadanfz <18221103@std.stei.itb.ac.id>
2024-12-03 15:29:44 +0700

    style: fix margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ function Home() {
     <div className="flex items-center justify-center">
       <div className="pt-4 text-center dark:bg-black">
         <h1 className="text-4xl font-bold dark:text-white">Coming Soon!</h1>
-        <div className="text-md dark:text-white">In development</div>
-        <p className="text-sm dark:text-white">- by Arkavidia 9.0 IT Team</p>
+        <div className="text-md mb-1 dark:text-white">In development</div>
+        <p className="mt-2 text-sm dark:text-white">- by Arkavidia 9.0 IT Team</p>
       </div>
     </div>
   )
