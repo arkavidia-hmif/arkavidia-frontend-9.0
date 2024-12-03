ab922f4da77a7a7e44e10f5f0b674331bebb300f
rasyadanfz <18221103@std.stei.itb.ac.id>
2024-12-03 14:33:49 +0700

    feat: add in development page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="pt-4 text-center">
+    <div className="pt-4 text-center dark:bg-black">
       <h1 className="text-4xl font-bold dark:text-white">Coming Soon!</h1>
-      <div className="text-md dark:text-white">In development</div>
+      <div className="text-md mb-2 dark:text-white">In development</div>
+      <p className="mt-2 text-sm">- by IT Arkavidia 9.0</p>
     </div>
   )
 }
