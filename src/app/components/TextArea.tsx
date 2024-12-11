2e9bce793981c5d1a009a274fa82b7d84639537c
TazakiN <tazkia.nizami@gmail.com>
2024-12-11 14:37:40 +0700

    feat: add helper text and improved styling according to state

--- src/app/components/TextArea.tsx
+++ src/app/components/TextArea.tsx
@@ -4,16 +4,18 @@ import { Textarea } from './ui/textarea'
 interface TextAreaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   label?: string
   state?: 'default' | 'error' | 'success'
+  helperText?: string
   value: string
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
 }
 
 function TextArea({
-  label = 'label',
+  label = 'Label',
   value,
   state = 'default',
   onChange,
   required,
+  helperText = 'Helper',
   ...props
 }: TextAreaProps) {
   const borderColor =
@@ -21,22 +23,26 @@ function TextArea({
       ? 'border-red-500'
       : state === 'success'
         ? 'border-green-500'
-        : 'border-gray-300'
+        : 'border-purple-400'
+
   return (
-    <div className="flex flex-col gap-1 text-left font-dmsans">
+    <div className="flex w-full flex-col gap-1 text-left font-dmsans">
       <label>
         <span className="text-xl text-lilac-100">{label}</span>
         {required && <span className="text-red-500"> *</span>}
       </label>
       <Textarea
-        className={`rounded-md border-2 p-2 ${borderColor} border-purple-400 bg-purple-100 text-purple-400`}
+        className={`rounded-md border-2 p-2 ${borderColor} bg-purple-100 text-purple-400`}
         value={value}
         onChange={onChange}
         placeholder="Type Something Here"
         required={required}
         {...props}
       />
-      <label className="text-sm text-purple-100">Helper</label>
+      <label
+        className={`text-sm ${state === 'error' ? 'text-red-500' : state === 'success' ? 'text-green-500' : 'text-purple-100'}`}>
+        {helperText}
+      </label>
     </div>
   )
 }
