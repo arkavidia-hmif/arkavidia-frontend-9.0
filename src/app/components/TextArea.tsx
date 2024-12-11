fc66fe3ccc9536fd7ca35b849d353533a75af454
TazakiN <tazkia.nizami@gmail.com>
2024-12-12 01:05:36 +0700

    fix: update text and border styling on states variants

--- src/app/components/TextArea.tsx
+++ src/app/components/TextArea.tsx
@@ -23,18 +23,20 @@ function TextArea({
     // diatur w-full supaya ngatur lebar textarea sesuai parentnya
     <div className="flex w-full flex-col gap-1 text-left font-dmsans">
       <label>
-        <span className="text-xl text-lilac-100">{label}</span>
+        <span className={`text-xl ${disabled ? 'text-neutral-200' : 'text-lilac-100'}`}>
+          {label}
+        </span>
         {required && <span className="text-red-500"> *</span>}
       </label>
       <Textarea
-        className={`rounded-md border-2 p-2 ${
+        className={`rounded-md border-2 p-2 text-purple-800 ${
           state === 'error'
-            ? 'border-red-500 text-red-500'
+            ? '!border-red-500'
             : state === 'success'
-              ? 'border-green-500 text-green-500'
+              ? '!border-green-500'
               : state === 'warn'
-                ? 'border-yellow-500 text-yellow-500'
-                : 'border-purple-400 text-purple-800'
+                ? '!border-yellow-500'
+                : 'border-purple-400'
         } bg-lilac-100 placeholder-opacity-100 placeholder:text-purple-400 hover:border-purple-300 hover:bg-lilac-200 focus:border-purple-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-purple-200 focus-visible:ring-opacity-50 disabled:cursor-not-allowed disabled:border-neutral-650 disabled:bg-neutral-200 disabled:opacity-50 disabled:hover:bg-neutral-300`}
         value={value}
         onChange={onChange}
@@ -45,13 +47,15 @@ function TextArea({
       {helperText && (
         <label
           className={`text-pretty text-sm ${
-            state === 'error'
-              ? 'text-red-500'
-              : state === 'success'
-                ? 'text-green-500'
-                : state === 'warn'
-                  ? 'text-yellow-500'
-                  : 'text-purple-100'
+            disabled
+              ? 'text-neutral-200'
+              : state === 'error'
+                ? 'text-red-500'
+                : state === 'success'
+                  ? 'text-green-500'
+                  : state === 'warn'
+                    ? 'text-yellow-500'
+                    : 'text-purple-100'
           }`}>
           {helperText}
         </label>
