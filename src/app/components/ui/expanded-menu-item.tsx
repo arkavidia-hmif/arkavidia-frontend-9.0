0303d6c2008859f265471cb00d54ee41b0756b2b
Shafiq Irvansyah <68421940+shafiqIrv@users.noreply.github.com>
2024-12-16 10:51:38 +0700

    feat: expanded menu item

---
+++ src/app/components/ui/expanded-menu-item.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from 'next/image'
+import { Checkbox } from './checkbox'
+
+interface ExpandedMenuItemProps {
+  iconLeft?: boolean
+  iconRight?: boolean
+  Option: string
+  icon?: boolean // This prop is defined but not used. Remove it if not needed.
+}
+
+function ExpandedMenuItem({ iconLeft, iconRight, Option }: ExpandedMenuItemProps) {
+  return (
+    <div className="flex w-[350px] items-center justify-between rounded-sm py-3 pl-3 pr-4 transition-all duration-100 ease-linear hover:bg-purple-100">
+      <div className="flex items-center gap-3">
+        {iconLeft && (
+          <Image
+            className="h-8 w-8"
+            src="/star.svg"
+            alt="star"
+            width={100}
+            height={100}
+          />
+        )}
+        <span className="font-dmsans text-xl text-purple-500">{Option}</span>
+      </div>
+      {iconRight && <Checkbox className="h-6 w-6 rounded-lg" />}
+    </div>
+  )
+}
+
+export default ExpandedMenuItem
