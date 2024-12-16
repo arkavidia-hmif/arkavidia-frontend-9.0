d084592670a64186e8c4beca08f0806195a9fdba
TazakiN <tazkia.nizami@gmail.com>
2024-12-16 17:21:32 +0700

    feat: update Navbar with profile dropdown and responsive adjustments

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { Button } from './ui/button'
 import Image from 'next/image'
-import { Menu } from 'lucide-react'
+import { LogOut, Menu } from 'lucide-react'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -42,22 +42,24 @@ function Navbar() {
             alt="Text Arkavidia 9.0"
             width={161}
             height={56}
-            className="hidden sm:block"
+            className="hidden md:block"
           />
         </div>
 
+        {/* Mobile menu */}
         <DropdownMenu>
-          <DropdownMenuTrigger className="rounded-md data-[state=open]:bg-purple-700 lg:hidden">
+          <DropdownMenuTrigger className="rounded-md data-[state=open]:bg-purple-700 md:hidden">
             <Menu size={24} className="m-3" color="white" />
           </DropdownMenuTrigger>
-          <DropdownMenuContent className="mr-2 min-w-[167px] gap-12 rounded-lg bg-purple-700 px-3 py-5 font-teachers text-base font-bold text-white outline-none">
+          <DropdownMenuContent className="gap- mr-2 min-w-[167px] rounded-lg border-none bg-purple-700 px-3 py-5 font-teachers text-base font-bold text-white">
             <DropdownMenuItem>About Us</DropdownMenuItem>
             <DropdownMenuItem>Event</DropdownMenuItem>
             <DropdownMenuItem>Competition</DropdownMenuItem>
             <DropdownMenuSeparator className="bg-transparent" />
-            <DropdownMenuItem>
+            <DropdownMenuItem className="focus:bg-transparent">
+              {/* // * Nanti diganti button dari component bagas */}
               <Link href="/login" className="w-full">
-                <Button className="h-auto w-full gap-4 rounded-lg bg-gradient-to-r from-teal-500 via-[#9274FF] to-[#C159D8] px-4 py-2 text-center font-dmsans text-base">
+                <Button className="h-auto w-full gap-4 rounded-lg bg-gradient-to-r from-teal-500 via-[#9274FF] to-[#C159D8] px-4 py-2 text-center font-dmsans text-xl font-semibold">
                   Login
                 </Button>
               </Link>
@@ -66,7 +68,7 @@ function Navbar() {
         </DropdownMenu>
 
         {/* Desktop menu */}
-        <ul className="hidden items-center justify-center space-x-8 text-white lg:flex">
+        <ul className="hidden items-center justify-center space-x-8 text-white md:flex">
           {NAV_ITEMS.map((item, index) => (
             <li key={index}>
               <Link
@@ -79,15 +81,28 @@ function Navbar() {
             </li>
           ))}
           {LOGGED_IN ? (
-            <Link href={'/profile'}>
-              <Image
-                src={'/profileLogo.svg'}
-                alt={'Profile Logo'}
-                width={30}
-                height={30}
-              />
-            </Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Image
+                  src={'/profileLogo.svg'}
+                  alt={'Profile Logo'}
+                  width={30}
+                  height={30}
+                  className="cursor-pointer rounded-full"
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="mr-2 mt-2 min-w-[155px] gap-12 rounded-lg border-none bg-purple-700 px-3 py-5 font-teachers text-base font-bold text-white">
+                <DropdownMenuItem className="cursor-pointer focus:bg-purple-600 focus:text-white">
+                  Dashboard
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer text-red-500 focus:bg-purple-600 focus:text-red-400">
+                  <LogOut className="mr-2 h-4 w-4" />
+                  Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           ) : (
+            // * Nanti diganti button dari component bagas
             <Link href="/login">
               <Button className="font-base h-auto gap-4 rounded-md bg-gradient-to-r from-teal-500 via-[#9274FF] to-[#C159D8] px-8 py-4 font-dmsans text-base">
                 Login
