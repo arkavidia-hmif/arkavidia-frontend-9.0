4f30de9fd885bf8798452bafa909be17f5ab7517
Shafiq Irvansyah <68421940+shafiqIrv@users.noreply.github.com>
2024-12-16 11:04:01 +0700

    fix: expanded menu item list

--- src/app/components/ui/expanded-menu-item.tsx
+++ src/app/components/ui/expanded-menu-item.tsx
@@ -7,11 +7,18 @@ interface ExpandedMenuItemProps {
   iconRight?: boolean
   Option: string
   icon?: boolean // This prop is defined but not used. Remove it if not needed.
+  disabled?: boolean
 }
 
-function ExpandedMenuItem({ iconLeft, iconRight, Option }: ExpandedMenuItemProps) {
+function ExpandedMenuItem({
+  iconLeft,
+  iconRight,
+  Option,
+  disabled = false
+}: ExpandedMenuItemProps) {
   return (
-    <div className="flex w-[350px] items-center justify-between rounded-sm py-3 pl-3 pr-4 transition-all duration-100 ease-linear hover:bg-purple-100">
+    <div
+      className={`flex w-[350px] items-center justify-between rounded-sm py-3 pl-3 pr-4 transition-all duration-100 ease-linear ${disabled ? 'cursor-not-allowed bg-gray-200' : 'hover:bg-purple-100'}`}>
       <div className="flex items-center gap-3">
         {iconLeft && (
           <Image
@@ -22,7 +29,12 @@ function ExpandedMenuItem({ iconLeft, iconRight, Option }: ExpandedMenuItemProps
             height={100}
           />
         )}
-        <span className="font-dmsans text-xl text-purple-500">{Option}</span>
+        <span
+          className={`font-dmsans text-xl ${
+            disabled ? 'text-gray-400' : 'text-purple-500'
+          }`}>
+          {Option}
+        </span>
       </div>
       {iconRight && <Checkbox className="h-6 w-6 rounded-lg" />}
     </div>
