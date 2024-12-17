ad6fed8c0855002fe1b5eeb4ff2fdfa8db67e084
bagassambega <bagassambega@gmail.com>
2024-12-17 09:25:20 +0700

    fix: outline add gradient

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,7 +1,9 @@
-import * as React from "react"
-import { Button as RadixButton, ButtonProps as RadixButtonProps } from "./ui/button"
-import { cn } from "~/lib/utils"
-import { cva } from "class-variance-authority"
+"use client";
+
+import * as React from "react";
+import { Button as RadixButton, ButtonProps as RadixButtonProps } from "./ui/button";
+import { cn } from "~/lib/utils";
+import { cva } from "class-variance-authority";
 
 const ButtonVariants = cva(
   "relative inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm " +
@@ -13,8 +15,8 @@ const ButtonVariants = cva(
         default:
           "bg-gradient-to-r from-[#48E6FF] via-[#9274FF] to-[#C159D8] text-white " +
           "hover:shadow-[0px_4px_15px_0px_#CE6AFF] transition-shadow duration-300 ease-out " +
-          "focus:border-[#CE6AFF] focus:outline-none focus:border-[2px] focus:shadow-[0px_4px_15px_0px_#CE6AFF] " +
-          "active:bg-[#CE6AFF]/[0.96] active:bg-none active:shadow-[0px_4px_15px_0px_#CE6AFF] " +
+          "focus:outline-none focus:border-[2px] focus:shadow-[0px_4px_15px_0px_#CE6AFF] " +
+          "active:bg-[#CE6AFF]/[0.96] active:shadow-[0px_4px_15px_0px_#CE6AFF] " +
           "disabled:bg-[#CCCCCC] disabled:bg-none disabled:text-white disabled:shadow-none",
         ghost:
           "bg-primary text-white " +
@@ -23,10 +25,11 @@ const ButtonVariants = cva(
           "active:bg-gray-700 " +
           "disabled:bg-[#CCCCCC] disabled:text-white ",
         outline:
+          "px-0 py-0 " +
           "hover:shadow-[0px_4px_15px_0px_#CE6AFF] transition-all duration-300 ease-out " +
           "focus:outline-none focus:shadow-[0px_4px_15px_0px_#CE6AFF] " +
-          "active:bg-[#CE6AFF] active:shadow-[0px_4px_15px_0px_#CE6AFF] " +
-          "disabled:text-[#CCCCCC] disabled:cursor-not-allowed disabled:shadow-none " ,
+          "active:bg-[#CE6AFF]/50 active:shadow-[0px_4px_15px_0px_#CE6AFF] " +
+          "disabled:text-[#CCCCCC] disabled:cursor-not-allowed disabled:shadow-none ",
         secondary:
           "bg-[#CE6AFF] text-white " +
           "hover:bg-[#A555CC] transition-color duration-300 ease-out " +
@@ -59,42 +62,53 @@ const ButtonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
-interface ButtonProps extends Omit<RadixButtonProps, 'size' | 'variant'> {
-  size?: "default" | "sm" | "lg" | "icon" | "xl" | "md" | "xs" | undefined | null
-  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | undefined | null
+interface ButtonProps extends Omit<RadixButtonProps, "size" | "variant"> {
+  size?: "default" | "sm" | "lg" | "icon" | "xl" | "md" | "xs" | undefined | null;
+  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | undefined | null;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size = "default", ...props }, ref) => {
-    const inlineStyles = variant === "outline" ?
-      {
-        border: "2px solid transparent",
-        background: "linear-gradient(black, black) padding-box, linear-gradient(113.98deg, #48E6FF -34.84%, #9274FF 45.46%, #C159D8 125.76%) border-box",
-      }
-      : {}
+    const [isActive, setIsActive] = React.useState(false);
+
+    const inlineStyles =
+      variant === "outline"
+        ? {
+          border: isActive ? "2px solid #CE6AFF" : "2px solid transparent", // Red border when active
+          background: !isActive
+            ? "linear-gradient(black, black) padding-box, linear-gradient(113.98deg, #48E6FF -34.84%, #9274FF 45.46%, #C159D8 125.76%) border-box"
+            : undefined,
+        }
+        : {};
 
     return (
       <RadixButton
         ref={ref}
         className={cn(ButtonVariants({ variant, size, className }))}
         style={inlineStyles}
+        onMouseDown={() => setIsActive(true)}
+        onMouseUp={() => setIsActive(false)}
+        onMouseLeave={() => setIsActive(false)}
         {...props}
       >
         {variant === "outline" ? (
-          <div className="relative bg-clip-text text-transparent bg-gradient-to-r from-[#48E6FF] via-[#9274FF] to-[#C159D8]">
+          <div
+            className={`relative bg-clip-text text-transparent  ${
+              isActive ? "bg-white" : "bg-gradient-to-r from-[#48E6FF] via-[#9274FF] to-[#C159D8]"
+            }`}
+          >
             {props.children}
           </div>
         ) : (
           props.children
         )}
       </RadixButton>
-    )
+    );
   }
-)
+);
 
-Button.displayName = "Button"
+Button.displayName = "Button";
 
-export { Button, ButtonVariants }
-
+export { Button, ButtonVariants };
