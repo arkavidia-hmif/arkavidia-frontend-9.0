9c3b871c13c5a580a05cf1680f3abd8e35531bf6
qiewi <105619444+qiewi@users.noreply.github.com>
2024-12-20 16:57:19 +0700

    feat: reset pass implementation init

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import { InputArea } from '../components/reset-password/input-area'
+import { Toaster } from '../components/ui/toaster'
+
+const ResetPasswordPage = () => {
+  return (
+      <div className="relative flex min-h-screen items-center bg-purple-900">
+        {/* Background Placeholder */}
+        <Image
+          src={'/images/reset-password/bg.svg'}
+          alt="Starry Background"
+          width={100}
+          height={100}
+          className="absolute h-screen w-screen"></Image>
+
+        {/* Main Page Parts */}
+        <div className="flex h-full w-full flex-row justify-center gap-[96px] max-md:my-10 max-md:flex-col max-md:items-center max-md:gap-[48px]">
+          <Image
+            src={'/images/reset-password/arkav-logo.png'}
+            alt={'Arkav Logo'}
+            width={503}
+            height={579}
+            className="z-10 max-lg:w-80 max-md:h-72 max-md:w-64"
+          />
+          <div className="z-10">
+            <InputArea />
+          </div>
+          {/* Background Placeholder */}
+        </div>
+        {/* NOTE: Might consider put this on main pages */}
+        <Toaster/>
+      </div>
+  )
+}
+
+export default ResetPasswordPage
