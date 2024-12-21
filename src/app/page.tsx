a99f1cdeaff8565673be6cce3ac1747ea3c78da0
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-12-22 00:08:15 +0700

    feat: add toggle component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
+import { Toggle } from './components/Toggle'
 
 function Home() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleToggleChange = (checked: boolean) => {
+    setIsChecked(checked); 
+  };
+
   return (
     <div className="flex h-full w-full items-center justify-center dark:bg-black">
       <div className="pt-4 text-center dark:bg-black">
         <h1 className="text-4xl font-bold dark:text-white">Coming Soon!</h1>
         <div className="text-md mb-1 dark:text-white">In development</div>
         <p className="mt-2 text-sm dark:text-white">- by Arkavidia 9.0 IT Team</p>
+        <Toggle
+          label="Placeholder"
+          checked={isChecked}
+          onChange={handleToggleChange}
+          labelPosition="right"
+          disabled={false}
+        />
       </div>
     </div>
   )
