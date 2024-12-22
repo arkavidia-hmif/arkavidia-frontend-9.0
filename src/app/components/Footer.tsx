567a83bba37ab516996a28f0ae2998ed252d3e39
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-12-22 10:59:24 +0700

    style: add background

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='flex flex-col items-start justify-center space-y-5'>
+    <div className='bg-[#0F0123] overflow-hidden relative'>
+      <div className='flex flex-row space-x-0 overflow-visible'>
+        <div className="absolute -bottom-5 md:-bottom-[45%] -left-[25%] md:-left-10 w-60 h-60 md:w-80 md:h-80 rounded-full bg-[#0F0123] p-0 shadow-[0_0_30px_rgba(239,211,211,0.4)]">
+          <div className="w-full h-full bg-[#0F0123] rounded-full"></div>
+        </div>
+        <img src='Vector 5.svg' alt='wave1' className='absolute bottom-0 left-0 lg:-left-1 z-0 h-[90%] w-full'/>
+        <img src='Vector 17.svg' alt='wave2' className='absolute bottom-0 right-0 lg:-right-[15%] z-0 h-[70%] w-full'/>
+        <div
+          className="absolute top-0 left-[30%] md:left-[20%] z-0 h-full w-full"
+          style={{
+            backgroundImage: "url('Stars.svg')",
+            backgroundRepeat: "repeat-x",
+            backgroundSize: "contain",
+          }}
+        />
+      </div>
+      <div className='relative z-10 flex flex-col items-start justify-center space-y-5 p-8 z-0'> 
         <img src='/Logos.svg' alt='Arkavidia Logo' className='w-[150px] h-[40px] object-contain' />
-        <div className='flex flex-row items-center justify-center space-x-5 font-teachers font-bold text-white text-sm'>
+        <div className='flex flex-row items-center justify-center space-x-4 font-teachers font-bold text-white text-sm'>
           <a href='/about-us' className='hover:underline'>About Us</a>
           <a href='/events' className='hover:underline'>Events</a>
           <a href='/competitions' className='hover:underline'>Competitions</a>
@@ -21,6 +37,7 @@ function Footer() {
           </a>
         </div>
         <p className='text-white text-sm'>© 2024 ARKAVIDIA. All rights reserved.</p>
+      </div>
     </div>
   )
 }
