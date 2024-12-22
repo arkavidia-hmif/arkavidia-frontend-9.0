2eb137f3b3840c81a08b7a5113880ac7d4a6c9b4
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-12-22 12:31:45 +0700

    feat: add toggle component

---
+++ src/app/components/Toggle.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Switch } from './ui/switch'
+
+interface ToggleProps {
+  label?: string
+  checked?: boolean
+  onChange?: (checked: boolean) => void
+  disabled?: boolean
+  labelPosition?: 'left' | 'right'
+}
+
+function Toggle({ label, checked, onChange, disabled, labelPosition }: ToggleProps) {
+  const handleCheckedChange = (checked: boolean) => {
+    if (onChange) {
+      onChange(checked);
+    }
+  };
+  return (
+    <div className="flex items-center gap-2">
+      {labelPosition === 'left' && <span className="text-white peer-disabled:opacity-50 font-dmsans">{label}</span>}
+      
+      <Switch 
+        checked={checked} 
+        onCheckedChange={handleCheckedChange}
+        disabled={disabled} />
+      
+      {labelPosition === 'right' && <span className="text-white peer-disabled:opacity-50 font-dmsans">{label}</span>}
+    </div>
+  )
+}
+export default Toggle
