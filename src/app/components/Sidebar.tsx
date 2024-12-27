835b1e7306a5298233ed7b557af2f1e8da97e84b
TazakiN <tazkia.nizami@gmail.com>
2024-12-27 20:25:27 +0700

    feat: add SVG icons for sidebar items and enhance Sidebar component with announcement toggle

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -11,7 +11,11 @@ import {
   DropdownMenuTrigger
 } from './ui/dropdown-menu'
 
-function Sidebar() {
+export interface SidebarProps {
+  announcement: boolean
+}
+
+function Sidebar({ announcement = true }: SidebarProps) {
   const SIDEBAR_ITEMS = [
     { name: 'Dashboard', link: '/dashboard' },
     { name: 'Competitive Programming', link: '/dashboard/cp' },
@@ -30,7 +34,7 @@ function Sidebar() {
 
   return (
     <div
-      className="fixed left-0 top-0 m-2 h-[calc(100vh-1rem)] w-full overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat lg:w-[156px]"
+      className="fixed left-0 top-0 m-2 h-[calc(100vh-1rem)] w-full overflow-visible rounded-xl bg-cover bg-center bg-no-repeat lg:w-[156px]"
       style={{ backgroundImage: "url('/images/sidebar/bg.svg')" }}>
       <div className="absolute inset-0 flex flex-col justify-between overflow-hidden">
         <div className="relative my-4 flex h-full w-full flex-col items-center justify-start lg:my-6">
@@ -41,10 +45,23 @@ function Sidebar() {
             height={100}
             className="lg:h-[138px] lg:w-[138px]"
           />
-          <div className="mt-4 flex w-full flex-col gap-3 px-2 lg:mt-7 lg:gap-3 lg:px-3">
+          <div className="mt-4 flex w-full flex-col gap-3 px-2 lg:mt-7 lg:gap-3 lg:px-[10px]">
             {SIDEBAR_ITEMS.map((item, index) => (
               <SidebarItem key={index} name={item.name} link={item.link} />
             ))}
+            {announcement ? (
+              <SidebarItem
+                name="Announcement"
+                link="/dashboard/announcement"
+                image="/images/sidebar/notification-important.svg"
+              />
+            ) : (
+              <SidebarItem
+                name="Profile"
+                link="/profile"
+                image="/images/sidebar/face.svg"
+              />
+            )}
           </div>
         </div>
 
@@ -66,19 +83,15 @@ function Sidebar() {
           </DropdownMenuTrigger>
           <DropdownMenuContent
             align="start"
-            className="m-auto ml-2 rounded-md bg-gradient-to-r from-purple-500 to-blue-600 p-2 shadow-[0_0_10px] shadow-lilac-200">
+            className="m-auto ml-1 rounded-md bg-gradient-to-r from-purple-500 to-blue-600 p-2 shadow-[0_0_10px] shadow-lilac-200">
             <DropdownMenuItem className="cursor-pointer rounded-lg text-white focus:text-white/80">
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M11.17 0H2C0.9 0 0 0.9 0 2V16C0 17.1 0.9 18 2 18H16C17.1 18 18 17.1 18 16V6.83C18 6.3 17.79 5.79 17.41 5.42L12.58 0.59C12.21 0.21 11.7 0 11.17 0ZM5 12H13C13.55 12 14 12.45 14 13C14 13.55 13.55 14 13 14H5C4.45 14 4 13.55 4 13C4 12.45 4.45 12 5 12ZM5 8H13C13.55 8 14 8.45 14 9C14 9.55 13.55 10 13 10H5C4.45 10 4 9.55 4 9C4 8.45 4.45 8 5 8ZM5 4H10C10.55 4 11 4.45 11 5C11 5.55 10.55 6 10 6H5C4.45 6 4 5.55 4 5C4 4.45 4.45 4 5 4Z"
-                  fill="white"
-                />
-              </svg>
+              <Image
+                src="/images/sidebar/landing-page.svg"
+                alt={'Landing Pace Icon'}
+                width={16}
+                height={18}
+                className="mr-2 h-4 w-4"
+              />
               <Link href="/">Landing Page</Link>
             </DropdownMenuItem>
             <DropdownMenuItem
@@ -102,7 +115,11 @@ interface SidebarItemProps {
   image?: string // * Kalo ada image, bisa langsung masukin aja di SIDEBAR_ITEMS
 }
 
-const SidebarItem: React.FC<SidebarItemProps> = ({ name, link, image }) => {
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  name,
+  link,
+  image = '/images/sidebar/item.svg'
+}) => {
   const pathname = usePathname()
   const isActive = pathname === link
 
@@ -116,15 +133,15 @@ const SidebarItem: React.FC<SidebarItemProps> = ({ name, link, image }) => {
       }`}>
       <div className="flex size-3 flex-shrink-0 items-center justify-center lg:size-4">
         <Image
-          src={image ?? '/images/sidebar/item.svg'}
+          src={image}
           alt="Arrow Right"
           width={10}
           height={10}
-          className="lg:h-3 lg:w-3"
+          className="lg:h-4 lg:w-4"
         />
       </div>
       <span
-        className={`flex-grow font-teachers text-xs font-bold text-white lg:text-base ${
+        className={`min-w-0 whitespace-normal break-words font-teachers text-xs font-bold text-white lg:text-base ${
           isActive ? 'drop-shadow-[0_0_4px_rgba(255,255,255,0.8)]' : ''
         }`}>
         {name}
