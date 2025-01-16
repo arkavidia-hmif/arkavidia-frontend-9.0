4ef7154993cba891fd52444a49f93a56f0e30d9c
rasyadanfz <18221103@std.stei.itb.ac.id>
2025-01-16 18:46:36 +0700

    feat: add google auth integration

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function expandCompetitionName(competitionName: string) {
+  switch (competitionName) {
+    case 'CP':
+      return 'Competitive Programming'
+    case 'CTF':
+      return 'Capture The Flag'
+    default:
+      return competitionName
+  }
 }
