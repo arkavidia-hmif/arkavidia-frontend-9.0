9d5d99ec42fcb12b8a80132aba83ae9536693f5e
shafiqIrv <shafiq.irvansyah@gmail.com>
2025-01-16 21:24:09 +0700

    fix: dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,26 @@
 import Sidebar from '../components/Sidebar'
+import Image from 'next/image'
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <section
-      className="py-6 md:min-h-screen"
-      style={{
-        backgroundImage: "url('/images/profile/bg.png')",
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat'
-      }}>
+      className="relative h-[100vh] min-h-screen w-full overflow-auto bg-gradient-to-b from-[#1f0246] to-[#2e046a] md:min-h-screen"
+      // style={{
+      //   backgroundImage: "url('/images/profile/bg.png')",
+      //   backgroundPosition: 'center',
+      //   backgroundRepeat: 'no-repeat'
+      // }}
+    >
+      <Image
+        src="/images/profile/reset-password/bg.png"
+        alt="Background"
+        className="fixed left-0 top-0 h-full w-full object-cover"
+        width={1920}
+        height={1080}
+      />
       <Sidebar />
-      <div className="px-8 lg:pl-[220px]">{children}</div>
+
+      <div className="px-5 pt-32 lg:pl-[220px] pb-5">{children}</div>
     </section>
   )
 }
