94eb43672e821254534fc0da6a34f579d23d0e16
TazakiN <tazkia.nizami@gmail.com>
2025-01-19 09:59:15 +0700

    fix: format deadline

--- src/app/components/admin-dashboard/FrameTugas.tsx
+++ src/app/components/admin-dashboard/FrameTugas.tsx
@@ -10,6 +10,15 @@ interface FrameTugasProps {
 
 const FrameTugas = ({ title, deadline, submitted, total }: FrameTugasProps) => {
   const progress = Math.round((submitted / total) * 10000) / 100
+  const formattedDeadline =
+    new Date(deadline).toLocaleString('id-ID', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    }) + ' WIB'
   return (
     <div className="from-white/24 to-white/8 flex flex-grow flex-col items-center justify-between gap-3 rounded-xl bg-gradient-to-r px-4 py-4 shadow-[0_0_8px_0_rgba(245,245,245,0.5)] md:flex-row md:px-6 md:py-5">
       <div className="flex flex-col">
@@ -17,7 +26,7 @@ const FrameTugas = ({ title, deadline, submitted, total }: FrameTugasProps) => {
           {title}
         </h3>
         <p className="text-center font-dmsans text-xs text-white md:text-left">
-          {deadline}
+          {formattedDeadline}
         </p>
       </div>
       <div className="w-full max-w-[100px] md:min-w-96">
