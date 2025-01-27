fe8fa3ae537670a1d2fea1524dd5099cd6fe7f78
bagassambega <bagassambega@gmail.com>
2025-01-27 20:49:03 +0700

    fix: get team submission data and payment proof

--- src/app/dashboard/admin/competition/[competitionID]/team/[teamID]/page.tsx
+++ src/app/dashboard/admin/competition/[competitionID]/team/[teamID]/page.tsx
@@ -4,40 +4,22 @@ import { Tab } from "~/app/components/Tab";
 import TeamInfo from "~/app/components/team-lists/detail/TeamInfo";
 import Submission from "~/app/components/team-lists/detail/Submission";
 import { useEffect, useState } from "react";
-import { getCompetitionById, getCompetitionSubmissionTeam, getTeamDetail, GetTeamDetailResponse } from "~/api/generated";
+import { getCompetitionById, getAdminCompetitionTeamInformation, GetAdminCompetitionTeamInformationResponse, TeamMemberDocument, UserDocument } from "~/api/generated";
 import useAxiosAuth from "~/lib/hooks/useAxiosAuth";
+import { useToast } from "~/hooks/use-toast";
 import { useParams, useRouter } from "next/navigation";
 import Hero from "~/app/components/team-lists/detail/Hero";
+import { getTeamStatus } from "~/app/components/registered-teamlist/teamlist";
 
 type Competition = 'CP' | 'CTF' | 'Hackvidia' | 'UXvidia' | 'Datavidia';
 
-const URL_PLACEHOLDER = 'https://picsum.photos/200/300'
 
-const payment_placeholder = {
-    url: URL_PLACEHOLDER,
-    typeID: '12'
-}
-
-const submission_placeholder = [
-    {
-        name: 'Participant 1',
-        studentCard: URL_PLACEHOLDER,
-        poster: URL_PLACEHOLDER,
-        twibbon: URL_PLACEHOLDER,
-    },
-    {
-        name: 'Participant 2',
-        studentCard: URL_PLACEHOLDER,
-        poster: URL_PLACEHOLDER,
-        twibbon: URL_PLACEHOLDER,
-    },
-    {
-        name: 'Participant 3',
-        studentCard: URL_PLACEHOLDER,
-        poster: URL_PLACEHOLDER,
-        twibbon: URL_PLACEHOLDER
-    }
-]
+type submissionsTypeID = {
+    name: string;
+    studentCard: string | null;
+    poster: string | null;
+    twibbon: string | null;
+}[];
 
 
 
@@ -45,9 +27,10 @@ function TeamDetails() {
     const router = useRouter();
     const params = useParams();
     const [competition, setCompetition] = useState<Competition>();
-    const [teamData, setTeamData] = useState<GetTeamDetailResponse>();
+    const [teamData, setTeamData] = useState<GetAdminCompetitionTeamInformationResponse>();
+    const [teamSubmission, setTeamSubmission] = useState<submissionsTypeID>()
     const axiosAuth = useAxiosAuth();
-
+    
     async function validateCompetition(value: string | null): Promise<boolean> {
         if (!value) {
             return false;
@@ -58,68 +41,141 @@ function TeamDetails() {
                 competitionId: value 
             }
         });
-
-        if (resp.error || !resp.data) {
+        
+        if (resp.error || !resp.data || resp.status !== 200) {
             return false;
         }
         setCompetition(resp.data.title as Competition);
-
-        // TODO: Uncomment this when the competition ID is finalized
-        return ['CP', 'CTF', 'Hackvidia', 'UXvidia', 'Datavidia'].includes(value as Competition);
+        
+        // Check by getCompetitionById is enough; if the getCompetitionById fails, that means no such competition ID
+        return true;
     }
-
-
+    
+    
+    const {toast} = useToast()
     useEffect(() => {
         const competitionParam = params.competitionID as string;
-
+        const teamIdParam = params.teamID as string;
+    
         if (!competitionParam || !validateCompetition(competitionParam)) {
-            router.push('/404');
+            toast({
+                title: 'Competition not found',
+                variant: 'destructive'
+            });
+            router.replace('/404');
             return;
         }
-
-        let isMounted = true;
-
+    
         const fetchTeamData = async () => {
-            const resp = await getTeamDetail({
+            const resp = await getAdminCompetitionTeamInformation({
                 client: axiosAuth,
                 path: {
-                    teamId: params.teamID as string,
+                    teamId: teamIdParam,
                     competitionId: competitionParam
                 }
             });
-            
+    
             if (resp.error || !resp.data) {
-                router.push('/404');
+                const responseError = resp.error;
+    
+                // Check if it's a ValidationError
+                if ('validationErrors' in responseError) {
+                    toast({
+                        title: 'Validation Error of fetching team data',
+                        description: 'Unknown validation error.',
+                        variant: 'destructive',
+                    });
+                } else if ('error' in responseError) {
+                    toast({
+                        title: 'Failed to fetch team data',
+                        description: responseError.error || 'An unknown error occurred.',
+                        variant: 'destructive',
+                    });
+                }
+                router.replace('/404');
                 return;
             }
-
-            if (isMounted) {
-                setTeamData(resp.data);
-            }
+    
+            setTeamData(resp.data);
         };
-
-        // TODO: Integrate
-        const fetchTeamSubmission = async () => {
-            const resp = await getCompetitionSubmissionTeam({
-                client: axiosAuth,
-                path: {
-                    teamId: params.teamID as string,
-                }
-            })
-        }
-
-
+    
         fetchTeamData();
-        fetchTeamSubmission();
-
-        return () => {
-            isMounted = false;
-        };
     }, [params, axiosAuth]);
+    
+    useEffect(() => {
+        if (!teamData) return;
+    
+        // Fetch submission data after `teamData` is loaded
+        const tempTeamSubmission: submissionsTypeID = [];
+        teamData.teamMembers?.forEach((member) => {
+            let latestPoster: TeamMemberDocument | null = null;
+            let latestTwibbon: TeamMemberDocument | null = null;
+    
+            const documents: TeamMemberDocument[] = Array.isArray(member.document)
+                ? member.document
+                : member.document
+                    ? [member.document] // Wrap a single object into an array
+                    : []; // Default to an empty array if null/undefined
+    
+            for (const doc of documents) {
+                if (doc.type === "poster") {
+                    if (
+                        !latestPoster ||
+                        new Date(doc.media.createdAt) > new Date(latestPoster.media.createdAt)
+                    ) {
+                        latestPoster = doc;
+                    }
+                } else if (doc.type === "twibbon") {
+                    if (
+                        !latestTwibbon ||
+                        new Date(doc.media.createdAt) > new Date(latestTwibbon.media.createdAt)
+                    ) {
+                        latestTwibbon = doc;
+                    }
+                }
+            }
+    
+            const tempMemberData = {
+                name: member.user?.fullName || '',
+                studentCard: (member.user?.document?.find((doc: UserDocument) => doc.type === "kartu-identitas")?.media.url)
+                    ? `https://${member.user?.document?.find((doc: UserDocument) => doc.type === "kartu-identitas")?.media.url}`
+                    : null,
+                twibbon: latestTwibbon?.media.url ? `https://${latestTwibbon.media.url}` : null,
+                poster: latestPoster?.media.url ? `https://${latestPoster.media.url}` : null,
+            };
+    
+            tempTeamSubmission.push(tempMemberData);
+        });
+    
+        setTeamSubmission(tempTeamSubmission);
+    }, [teamData]);
+    
 
     if (!teamData) {
         return ;
     }
+
+    // NOTE: since there could be several payment proof, this method only takes the latest payment proof uploaded
+    const paymentProof = (() => {
+        if (!teamData.document || teamData.document.length === 0) {
+          return { url: null, typeID: null };
+        }
+      
+        // Find the latest document based on the createdAt field
+        const latestDocument = teamData.document.reduce((latest, current) =>
+          new Date(current.media.createdAt) > new Date(latest.media.createdAt) ? current : latest
+        );
+      
+        // Extract and format the URL
+        const url = latestDocument.media.url.includes('http')
+          ? latestDocument.media.url
+          : `https://${latestDocument.media.url}`;
+      
+        return {
+          url: url || null,
+          typeID: latestDocument.type || null,
+        };
+      })();
 
     return (
         <div
@@ -134,8 +190,8 @@ function TeamDetails() {
             <Hero
                 teamName={teamData.name}
                 teamID={'#'+teamData.joinCode}
-                teamStatus={teamData.isVerified? 'Verified' : 'Unverified'}
-                teamStage={teamData.competitionStage} 
+                teamStatus={getTeamStatus(teamData)}
+                teamStage={teamData.stage} 
             />
             {competition && (
                 competition === 'UXvidia' || competition === 'Datavidia' ? (
@@ -145,10 +201,10 @@ function TeamDetails() {
                             <TeamInfo 
                                 key="team-info"
                                 members={teamData?.teamMembers}
-                                paymentProof={payment_placeholder}
+                                paymentProof={paymentProof}
                                 teamID={teamData.id}
-                                submissionsTypeID={submission_placeholder}
-                                existsSubmission                         
+                                submissionsTypeID={teamSubmission}
+                                existsSubmission
                             />,
                             <Submission key="submission" />
                         ]} 
@@ -160,8 +216,8 @@ function TeamDetails() {
                             <TeamInfo 
                                 key="team-info"
                                 members={teamData?.teamMembers}
-                                paymentProof={payment_placeholder}
-                                submissionsTypeID={submission_placeholder}   
+                                paymentProof={paymentProof}
+                                submissionsTypeID={teamSubmission}   
                                 teamID={teamData.id}                      
                             />,
                         ]} 
