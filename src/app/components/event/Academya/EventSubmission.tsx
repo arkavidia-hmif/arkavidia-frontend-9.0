ddcd184da7aacbfc19a2d2d907a7e294afb8e6e5
rasyadanfz <18221103@std.stei.itb.ac.id>
2025-02-19 20:58:42 +0700

    fix: type error

--- src/app/components/event/Academya/EventSubmission.tsx
+++ src/app/components/event/Academya/EventSubmission.tsx
@@ -58,7 +58,7 @@ export default function EventSubmission({}: {}) {
       return
     }
 
-    // @ts-expect-error
+    // @ts-ignore
     const responseData = response?.data as EventTeam
 
     setTeamData(responseData)
